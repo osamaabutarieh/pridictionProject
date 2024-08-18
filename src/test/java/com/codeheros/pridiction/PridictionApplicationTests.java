304eec41e64071280efd12883077c75c4fd1a58c
osamaabu-tariah <osamamarwan21@gmail.com>
2024-08-18 21:46:54 +0300

    first commit

---
+++ src/test/java/com/codeheros/pridiction/PridictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeheros.pridiction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PridictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
