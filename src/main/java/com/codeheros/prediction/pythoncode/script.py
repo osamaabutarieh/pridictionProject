a5434341838869b52e36a9cc05eac54021a02af0
Issam Abdoh <iss20190134@std.psut.edu.jo>
2024-08-18 22:56:57 +0300

    some python code

--- src/main/java/com/codeheros/prediction/pythoncode/script.py
+++ src/main/java/com/codeheros/prediction/pythoncode/script.py
@@ -1,1 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+
 print("test")
