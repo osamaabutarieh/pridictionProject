21d1e3d48fcda1ec7ce2a088d8c493a2f71af700
Issam Abdoh <iss20190134@std.psut.edu.jo>
2024-08-18 21:57:16 +0300

    testing message

--- src/main/java/com/codeheros/pridiction/PridictionApplication.java
+++ src/main/java/com/codeheros/pridiction/PridictionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PridictionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//hello from the other side
 		SpringApplication.run(PridictionApplication.class, args);
 	}
 
